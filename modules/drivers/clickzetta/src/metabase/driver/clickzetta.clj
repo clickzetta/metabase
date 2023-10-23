d4ac16b31e907b3c2e4332094718b638d59833fd
hanmiao.li <hanmiao.li@clickzetta.com>
2023-10-23 15:10:16 +0800

    fix literal quota bug

--- modules/drivers/clickzetta/src/metabase/driver/clickzetta.clj
+++ modules/drivers/clickzetta/src/metabase/driver/clickzetta.clj
@@ -45,7 +45,7 @@
 
 (driver/register! :clickzetta, :parent #{:sql-jdbc ::sql-jdbc.legacy/use-legacy-classes-for-read-and-set})
 
-(doseq [[feature supported?] {:datetime-diff                          false
+(doseq [[feature supported?] {:datetime-diff                          true
                               :now                                    true
                               :convert-timezone                       false
                               :connection-impersonation               false
@@ -157,43 +157,105 @@
   [_driver]
   2)
 
-(defmethod sql.qp/unix-timestamp->honeysql [:clickzetta :seconds]      [_ _ expr] [:to_unix_timestamp expr])
-(defmethod sql.qp/unix-timestamp->honeysql [:clickzetta :milliseconds] [_ _ expr] [:to_unix_timestamp expr 3])
-(defmethod sql.qp/unix-timestamp->honeysql [:clickzetta :microseconds] [_ _ expr] [:to_unix_timestamp expr 6])
-
 (defmethod sql.qp/current-datetime-honeysql-form :clickzetta
   [_]
   (h2x/with-database-type-info :%current_timestamp :TIMESTAMP_LTZ))
 
-(defmethod sql.qp/add-interval-honeysql-form :clickzetta
-  [_ hsql-form amount unit]
-  [:dateadd
-   [:raw (name unit)]
-   [:raw (int amount)]
-   (h2x/->timestamp hsql-form)])
-
-(defn- date-trunc [unit expr] [:date_trunc unit (h2x/->timestamp expr)])
-
-(defmethod sql.qp/date [:clickzetta :default]         [_ _ expr] expr)
-(defmethod sql.qp/date [:clickzetta :minute]          [_ _ expr] (date-trunc :minute expr))
-(defmethod sql.qp/date [:clickzetta :hour]            [_ _ expr] (date-trunc :hour expr))
-(defmethod sql.qp/date [:clickzetta :day]             [_ _ expr] (date-trunc :day expr))
-(defmethod sql.qp/date [:clickzetta :month]           [_ _ expr] (date-trunc :month expr))
-(defmethod sql.qp/date [:clickzetta :quarter]         [_ _ expr] (date-trunc :quarter expr))
-(defmethod sql.qp/date [:clickzetta :year]            [_ _ expr] (date-trunc :year expr))
-
-;; these don't need to be adjusted for start of week, since we're Setting the WEEK_START connection parameter
-(defmethod sql.qp/date [:clickzetta :week]
-  [_driver _unit expr]
-  (date-trunc :week expr))
-
-(defmethod sql.qp/date [:clickzetta :week-of-year-iso]
+(defmethod sql.qp/unix-timestamp->honeysql [:clickzetta :seconds]
   [_ _ expr]
-  (:weekofyear expr))
+  (h2x/->timestamp [:from_unixtime expr]))
+
+(defn- date-format [format-str expr]
+  [:date_format expr (h2x/literal format-str)])
+
+(defn- str-to-date [format-str expr]
+  (h2x/->timestamp [:from_unixtime [:unix_timestamp expr (h2x/literal format-str)]]))
+
+(defn- trunc-with-format [format-str expr]
+  (str-to-date format-str (date-format format-str expr)))
+
+(defmethod sql.qp/date [:clickzetta :default]         [_ _ expr] (h2x/->timestamp expr))
+(defmethod sql.qp/date [:clickzetta :minute]          [_ _ expr] (trunc-with-format "yyyy-MM-dd HH:mm" (h2x/->timestamp expr)))
+(defmethod sql.qp/date [:clickzetta :minute-of-hour]  [_ _ expr] [:minute (h2x/->timestamp expr)])
+(defmethod sql.qp/date [:clickzetta :hour]            [_ _ expr] (trunc-with-format "yyyy-MM-dd HH" (h2x/->timestamp expr)))
+(defmethod sql.qp/date [:clickzetta :hour-of-day]     [_ _ expr] [:hour (h2x/->timestamp expr)])
+(defmethod sql.qp/date [:clickzetta :day]             [_ _ expr] (trunc-with-format "yyyy-MM-dd" (h2x/->timestamp expr)))
+(defmethod sql.qp/date [:clickzetta :day-of-month]    [_ _ expr] [:dayofmonth (h2x/->timestamp expr)])
+(defmethod sql.qp/date [:clickzetta :day-of-year]     [_ _ expr] (h2x/->integer (date-format "D" (h2x/->timestamp expr))))
+(defmethod sql.qp/date [:clickzetta :month]           [_ _ expr] [:date_trunc (h2x/literal :MM) (h2x/->timestamp expr)])
+(defmethod sql.qp/date [:clickzetta :month-of-year]   [_ _ expr] [:month (h2x/->timestamp expr)])
+(defmethod sql.qp/date [:clickzetta :quarter-of-year] [_ _ expr] [:quarter (h2x/->timestamp expr)])
+(defmethod sql.qp/date [:clickzetta :year]            [_ _ expr] [:date_trunc  (h2x/literal :year) (h2x/->timestamp expr)])
+(defmethod sql.qp/date [:clickzetta :week]            [_ _ expr] [:date_trunc (h2x/literal :week) (h2x/->timestamp expr)])
+(defmethod sql.qp/date [:clickzetta ::quarter]        [_ _ expr] [:date_trunc (h2x/literal ::quarter) (h2x/->timestamp expr)])
+
 
 (defmethod sql.qp/date [:clickzetta :day-of-week]
   [_driver _unit expr]
-  (:weekday expr))
+  [:dayofweek (h2x/->timestamp expr)])
+
+
+(defmethod sql.qp/date [:clickzetta :week-of-year-iso]
+  [_driver _unit expr]
+  [:weekofyear (h2x/->timestamp expr)])
+
+(defmethod sql.qp/->honeysql [:clickzetta :replace]
+  [driver [_ arg pattern replacement]]
+  [:regexp_replace
+   (sql.qp/->honeysql driver arg)
+   (sql.qp/->honeysql driver pattern)
+   (sql.qp/->honeysql driver replacement)])
+
+(defmethod sql.qp/->honeysql [:clickzetta :regex-match-first]
+  [driver [_ arg pattern]]
+  [:regexp_extract (sql.qp/->honeysql driver arg) (sql.qp/->honeysql driver pattern) 0])
+
+(defmethod sql.qp/->honeysql [:clickzetta :median]
+  [driver [_ arg]]
+  [:percentile (sql.qp/->honeysql driver arg) 0.5])
+
+(defmethod sql.qp/->honeysql [:clickzetta :percentile]
+  [driver [_ arg p]]
+  [:percentile (sql.qp/->honeysql driver arg) (sql.qp/->honeysql driver p)])
+
+(defmethod sql.qp/add-interval-honeysql-form :clickzetta
+  [_ hsql-form amount unit]
+  [:dateadd
+     [:raw (name unit)]
+     [:raw (int amount)]
+     (h2x/->timestamp hsql-form)])
+
+(defmethod sql.qp/datetime-diff [:clickzetta :year]
+  [driver _unit x y]
+  [:div (sql.qp/datetime-diff driver :month x y) 12])
+
+(defmethod sql.qp/datetime-diff [:clickzetta :quarter]
+  [driver _unit x y]
+  [:div (sql.qp/datetime-diff driver :month x y) 3])
+
+(defmethod sql.qp/datetime-diff [:clickzetta :month]
+  [_driver _unit x y]
+  (h2x/->integer [:months_between y x]))
+
+(defmethod sql.qp/datetime-diff [:clickzetta :week]
+  [_driver _unit x y]
+  [:div [:datediff y x] 7])
+
+(defmethod sql.qp/datetime-diff [:clickzetta :day]
+  [_driver _unit x y]
+  [:datediff y x])
+
+(defmethod sql.qp/datetime-diff [:clickzetta :hour]
+  [driver _unit x y]
+  [:div (sql.qp/datetime-diff driver :second x y) 3600])
+
+(defmethod sql.qp/datetime-diff [:clickzetta :minute]
+  [driver _unit x y]
+  [:div (sql.qp/datetime-diff driver :second x y) 60])
+
+(defmethod sql.qp/datetime-diff [:clickzetta :second]
+  [_driver _unit x y]
+  [:- [:unix_timestamp y] [:unix_timestamp x]])
 
 (defmethod sql.qp/quote-style :clickzetta
   [_driver]
